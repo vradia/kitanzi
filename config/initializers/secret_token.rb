e8bae5e43a22a3b2b09bfa10736b10944afd3240
Vikash Radia <vikash@me.com>
2013-11-25 11:45:05 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kitanzi::Application.config.secret_key_base = '1cd6a8959094ff8e3de8d6b3f40ec149630447489e7fdc62d4facf23f6ec0c920ad1e9a75b80ed4d63a6fdf00d1cc706f38d7bcece656cddb794ba9847389f02'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kitanzi::Application.config.session_store :cookie_store, key: '_kitanzi_session'
